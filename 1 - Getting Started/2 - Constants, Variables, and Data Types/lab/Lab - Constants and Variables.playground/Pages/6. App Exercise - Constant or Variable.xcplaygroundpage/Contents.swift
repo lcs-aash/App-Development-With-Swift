abf3ea5e759ed3f2b9ba58e64722c4f189ed3db4
Rori Ash <aash@lcs.on.ca>
2019-09-30 11:09:06 -0400

    Fitness Tracker

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
@@ -11,11 +11,11 @@
  - Goal number of steps: The user's goal for number of steps to take each day
  - Average heart rate: The user's average heart rate over the last 24 hours
  */
-
-
-
-
-
+let name = "rori"
+var age = 15
+var NumberOfStepsTaken = 9000
+let StepGoal = 10000
+var AverageHeartRate = 80
 /*:
  Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
  */
