65098d2d7c0ebfc6df5e4a530cde163006db7e2c
Rori Ash <aash@lcs.on.ca>
2019-11-13 11:19:35 -0500

    Completed lab: String Basics

--- lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -3,8 +3,8 @@
  
  Create a `name` constant and assign it a string literal representing your name.
  */
-
-
+let name = "Rori"
+//String literal: Put quotation marks around text
 /*:
  Create a `favoriteQuote` constant and assign it the following string literal:
  
@@ -15,12 +15,15 @@
  - callout(Example): If your favorite quote is "The grass is always greener on the other side" the value of `favoriteQuote` should be such that printing `favoriteQuote` results in the following:
     * `My favorite quote is "The grass is always greener on the other side."`
  */
+// Putting a quote in a quote
+let favouriteQuote = "My favourite quote is\"It could've been worse.\""
+print(favouriteQuote)
 
-
-/*:
- Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
- */
 let emptyString = ""
-
+if emptyString.isEmpty "" {
+    print ("There's nothing here")
+} else { 
+    }
 
 //: page 1 of 5  |  [Next: Exercise - Concatenation and Interpolation](@next)
+}
--- lab/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
+++ lab/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
@@ -3,14 +3,21 @@
  
  Create a `city` constant and assign it a string literal representing your home city. Then create a `state` constant and assign it a string literal representing your home state. Finally, create a `home` constant and use string concatenation to assign it a string representing your home city and state (i.e. Portland, Oregon). Print the value of `home`.
  */
-
-
+let city = "Buckhorn"
+let province = "Ontario"
+let home = city + ", " + province
 /*:
  Use the compound assignment operator (`+=`) to add `home` to `introduction` below. Print the value of `introduction`.
  */
 var introduction = "I live in"
 
-
+//Example: WHen concatenation is not enough
+let declaration = "My favourite number is "
+let number = 10
+let sentence = declaration + number
+// Can not use the concatenation to mash strings and integers
+let sentence = "\declaration \(number)"
+print(sentence)
 /*:
  Declare a `name` constant and assign it your name as a string literal. Then declare an `age` constant and give it your current age as an `Int`. Then print the following phrase using string interpolation:
  
